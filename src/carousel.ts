d7588ee1b20a3715314e75a196de24c4577c1915
Kseniya <kseniya.panasyuk@gmail.com>
2017-08-27 22:54:08 +0200

    added slide navigation

--- src/carousel.ts
+++ src/carousel.ts
@@ -8,6 +8,8 @@ export class Carousel {
     private n: number;
     private index: number = 0;
     private indicator: Indicator;
+    private xDown: number;
+    private yDown: number;
 
     constructor(container: HTMLElement) {
         this.container = container;
@@ -27,6 +29,34 @@ export class Carousel {
             this.indicator = new Indicator(this.n).attachTo(this.container);
             new RightNavigator().attachTo(this.container).onNavigation(() => this.next());
             new LeftNavigator().attachTo(this.container).onNavigation(() => this.previous());
+            const carousel = this;
+            this.container.addEventListener('touchstart', (e) => {
+                carousel.xDown = e.touches[0].clientX;
+                carousel.yDown = e.touches[0].clientY;
+            }, false);
+
+            this.container.addEventListener('touchmove', (e) => {
+                if ( ! carousel.xDown || ! carousel.yDown ) {
+                    return;
+                }
+
+                let xUp = e.touches[0].clientX;
+                let yUp = e.touches[0].clientY;
+
+                let xDiff = carousel.xDown  - xUp;
+                let yDiff = carousel.yDown  - yUp;
+
+                if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+                    if ( xDiff > 0 ) {
+                        carousel.previous();
+                    } else {
+                        carousel.next();
+                    }
+                }
+
+                carousel.xDown = null;
+                carousel.yDown = null;
+            }, false);
         }
     }
 
